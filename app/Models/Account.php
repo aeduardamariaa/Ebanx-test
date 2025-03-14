7676128573931d308d33329f783944886a92761a
Maria Eduarda da Silva <silvamariaeduarda92@gmail.com>
2025-03-13 21:04:23 -0300

    feat: accounts model and migration created

---
+++ app/Models/Account.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    protected $table = 'accounts';
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id', 
+        'balance'
+    ];
+}
